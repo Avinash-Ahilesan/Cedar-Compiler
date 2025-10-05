8901c8348750e7d0d6f99735c481998270ed61c8
Avinash Ahilesan <avinash.ahilesan00@gmail.com>
2025-10-05 22:28:37 +0300

    First file i/o

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,13 @@
-let () = print_endline "Hello, World!"
+let file = "./test_code/test_code.cedar"
+
+
+let () = 
+    let ic = open_in file in 
+        try
+            let line = input_line ic in
+                print_endline line;
+                flush stdout;
+                close_in ic
+        with e ->
+            close_in_noerr ic;
+            raise e
